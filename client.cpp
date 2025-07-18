bd0ec2adf4f1d8bda739f492f074d605fd42b23b
Jan Andrei Reyes <167843080+LibrarianDarchivo@users.noreply.github.com>
2025-07-18 21:42:46 +0800

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,50 +1,44 @@
 #include <iostream>
 #include <string>
-#include <cstdlib>
-#include <unistd.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <ctime>
+#include <unistd.h>
 
-#define SERVER_IP "127.0.0.1"
-#define PORT 8080
-#define BUFFER_SIZE 1024
+#define PORT 12345
+
+using namespace std;
 
 int main() {
-    srand(time(0));
+    int sock = 0;
+    sockaddr_in serv_addr;
+    char buffer[4096] = {0};
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    sockaddr_in serv_addr{};
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-    inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr);
-
-    if (connect(sock, (sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        std::cerr << "Failed to connect to server\n";
-        return 1;
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        cerr << "Socket creation error\n";
+        return -1;
     }
 
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::getline(std::cin, name);
-    send(sock, name.c_str(), name.length(), 0);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
 
-    char buffer[BUFFER_SIZE];
-    while (true) {
-        memset(buffer, 0, BUFFER_SIZE);
-        int bytes = recv(sock, buffer, BUFFER_SIZE, 0);
-        if (bytes <= 0) break;
+    // CHANGE IP if needed
+    if (inet_pton(AF_INET, "192.168.254.143", &serv_addr.sin_addr) <= 0) {
+        cerr << "Invalid address\n";
+        return -1;
+    }
 
-        std::string msg(buffer);
-        if (msg == "ROUND_START") {
-            int card = (rand() % 13) + 2;
-            std::cout << "[You drew]: " << card << std::endl;
-            std::string cardMsg = "CARD:" + std::to_string(card);
-            send(sock, cardMsg.c_str(), cardMsg.length(), 0);
-        } else {
-            std::cout << msg << std::endl;
-            if (msg.find("GAME_OVER") != std::string::npos)
-                break;
-        }
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        cerr << "Connection Failed\n";
+        return -1;
+    }
+
+    cout << "Connected to server.\n";
+
+    // Receive hand
+    int valread = read(sock, buffer, sizeof(buffer));
+    if (valread > 0) {
+        cout << string(buffer, valread) << endl;
     }
 
     close(sock);

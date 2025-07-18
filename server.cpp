5bd4a8b8597520a5423c83496c44fdc0988f347e
Jan Andrei Reyes <167843080+LibrarianDarchivo@users.noreply.github.com>
2025-07-18 22:00:56 +0800

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,115 +1,123 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
-#include <random>
-#include <ctime>
 #include <thread>
-#include <netinet/in.h>
 #include <unistd.h>
-#include <cstring>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
 using namespace std;
 
 const int PORT = 12345;
 const int MAX_CLIENTS = 4;
 
 vector<int> client_sockets;
-vector<string> playerNames;
+vector<string> playerNames(MAX_CLIENTS);
+vector<int> playerScores(MAX_CLIENTS, 0);
 
-// Basic card setup
-vector<string> generateDeck() {
-    vector<string> suits = {"♠", "♥", "♦", "♣"};
-    vector<string> ranks = {"3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2"};
-    vector<string> deck;
-    for (const auto& s : suits) {
-        for (const auto& r : ranks) {
-            deck.push_back(r + s);
-        }
+void broadcast(const string &message) {
+    for (int sock : client_sockets) {
+        send(sock, message.c_str(), message.size(), 0);
     }
-    return deck;
 }
 
-void dealCards(vector<vector<string>>& hands) {
-    vector<string> deck = generateDeck();
-    shuffle(deck.begin(), deck.end(), default_random_engine(time(0)));
+void handleGame() {
+    srand(time(0));
+    string startMsg = "\n=== Pusoy Clash Game Simulation ===\n";
+    broadcast(startMsg);
+    sleep(1);
 
-    hands.resize(MAX_CLIENTS);
-    for (int i = 0; i < 52; ++i) {
-        hands[i % MAX_CLIENTS].push_back(deck[i]);
+    for (int round = 1; round <= 3; ++round) {
+        broadcast("\n--- Starting Round " + to_string(round) + " ---\n");
+        vector<int> cards(MAX_CLIENTS);
+
+        for (int i = 0; i < MAX_CLIENTS; ++i) {
+            cards[i] = rand() % 13 + 2;
+            broadcast(playerNames[i] + " draws a card: " + to_string(cards[i]) + "\n");
+            sleep(1);
+        }
+
+        int maxCard = *max_element(cards.begin(), cards.end());
+        vector<int> winners;
+        for (int i = 0; i < MAX_CLIENTS; ++i) {
+            if (cards[i] == maxCard) {
+                winners.push_back(i);
+            }
+        }
+
+        if (winners.size() == 1) {
+            playerScores[winners[0]]++;
+            broadcast("\nRound " + to_string(round) + " winner: " + playerNames[winners[0]] +
+                      " with card " + to_string(maxCard) + "!\n");
+        } else {
+            broadcast("\nRound " + to_string(round) + " is a tie!\n");
+        }
+        sleep(2);
+    }
+
+    broadcast("\n=== Final Scores ===\n");
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        broadcast(playerNames[i] + ": " + to_string(playerScores[i]) + " points\n");
+    }
+
+    int maxScore = *max_element(playerScores.begin(), playerScores.end());
+    vector<int> finalWinners;
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (playerScores[i] == maxScore) {
+            finalWinners.push_back(i);
+        }
+    }
+
+    if (finalWinners.size() == 1) {
+        broadcast("\n" + playerNames[finalWinners[0]] + " won with " +
+                  to_string(maxScore) + " points!\n");
+    } else {
+        broadcast("\nIt's a tie between:\n");
+        for (int i : finalWinners) {
+            broadcast("- " + playerNames[i] + "\n");
+        }
     }
 }
 
 int main() {
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == 0) {
-        cerr << "Socket creation error\n";
-        exit(EXIT_FAILURE);
-    }
-
-    sockaddr_in address;
+    int server_fd, new_socket;
+    struct sockaddr_in address;
     int opt = 1;
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    socklen_t addrlen = sizeof(address);
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
-
-    bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
     listen(server_fd, MAX_CLIENTS);
-    cout << "Server started on port " << PORT << endl;
 
-    // Accept all players
+    cout << "Server started on port " << PORT << ". Waiting for players...\n";
+
     while (client_sockets.size() < MAX_CLIENTS) {
-        int new_socket = accept(server_fd, nullptr, nullptr);
-        if (new_socket >= 0) {
-            client_sockets.push_back(new_socket);
-            char name_buf[100] = {0};
-            read(new_socket, name_buf, 100);
-            playerNames.push_back(string(name_buf));
-            cout << "Player connected: " << name_buf << endl;
-        }
+        new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
+
+        char nameBuffer[1024] = {0};
+        read(new_socket, nameBuffer, sizeof(nameBuffer));
+        string playerName(nameBuffer);
+
+        client_sockets.push_back(new_socket);
+        playerNames[client_sockets.size() - 1] = playerName;
+
+        string joinMsg = playerName + " has joined the game!\n";
+        cout << joinMsg;
+        broadcast(joinMsg);
     }
 
-    cout << "All players connected. Game is starting!\n";
+    broadcast("\nAll players connected. Game is starting!\n");
+    handleGame();
 
-    // Deal cards
-    vector<vector<string>> hands;
-    dealCards(hands);
-
-    // Send cards to players
-    for (int i = 0; i < MAX_CLIENTS; ++i) {
-        string hand = "Your hand:\n";
-        for (const string& card : hands[i]) {
-            hand += card + " ";
-        }
-        hand += "\n";
-        send(client_sockets[i], hand.c_str(), hand.size(), 0);
-    }
-
-    // Begin simple turn-based loop
-    int currentPlayer = 0;
-    while (true) {
-        string turnMsg = "It's your turn. Enter a card to play:\n";
-        send(client_sockets[currentPlayer], turnMsg.c_str(), turnMsg.size(), 0);
-
-        char buffer[1024] = {0};
-        int valread = read(client_sockets[currentPlayer], buffer, 1024);
-        if (valread <= 0) {
-            cout << playerNames[currentPlayer] << " disconnected.\n";
-            break;
-        }
-
-        string playedCard(buffer, valread);
-        string broadcast = playerNames[currentPlayer] + " played: " + playedCard + "\n";
-
-        for (int i = 0; i < MAX_CLIENTS; ++i) {
-            if (i != currentPlayer) {
-                send(client_sockets[i], broadcast.c_str(), broadcast.size(), 0);
-            }
-        }
-
-        currentPlayer = (currentPlayer + 1) % MAX_CLIENTS;
-    }
-
+    for (int sock : client_sockets) close(sock);
     close(server_fd);
     return 0;
 }
+
+rizz

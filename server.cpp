e1e97faea74339230284a097e037eb50ddde59e5
Jan Andrei Reyes <167843080+LibrarianDarchivo@users.noreply.github.com>
2025-07-18 21:42:32 +0800

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,40 +1,59 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <algorithm>
+#include <random>
 #include <thread>
 #include <mutex>
-#include <unistd.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
+#include <unistd.h>
 
 #define PORT 12345
 #define MAX_CLIENTS 4
 
 using namespace std;
 
-vector<int> clients;
-mutex clients_mutex;
+struct Card {
+    string suit;
+    string rank;
 
-void handle_client(int client_socket, int id) {
-    cout << "Player " << id + 1 << " connected.\n";
-    
-    string welcome = "Welcome Player " + to_string(id + 1) + "!\n";
-    send(client_socket, welcome.c_str(), welcome.size(), 0);
+    string toString() const {
+        return rank + " of " + suit;
+    }
+};
 
-    // Wait until game starts (could add gameplay here)
+vector<Card> generateDeck() {
+    vector<string> suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+    vector<string> ranks = {"3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2"};
+
+    vector<Card> deck;
+    for (const auto& suit : suits) {
+        for (const auto& rank : ranks) {
+            deck.push_back({suit, rank});
+        }
+    }
+
+    // Shuffle deck
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(deck.begin(), deck.end(), g);
+
+    return deck;
 }
 
-void broadcast_start() {
-    string message = "All players connected. Game is starting!\n";
-    lock_guard<mutex> lock(clients_mutex);
-    for (int sock : clients) {
-        send(sock, message.c_str(), message.size(), 0);
+void sendHand(int socket, const vector<Card>& hand) {
+    string msg = "Your hand:\n";
+    for (const auto& card : hand) {
+        msg += "- " + card.toString() + "\n";
     }
+    send(socket, msg.c_str(), msg.size(), 0);
 }
 
 int main() {
     int server_fd, new_socket;
     sockaddr_in address;
     socklen_t addrlen = sizeof(address);
+    vector<int> client_sockets;
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == 0) {
@@ -43,7 +62,7 @@ int main() {
     }
 
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;  // Listen on all interfaces
+    address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
     if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
@@ -56,30 +75,29 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    cout << "Server started on port " << PORT << ". Waiting for players...\n";
+    cout << "Server ready on port " << PORT << ". Waiting for players...\n";
 
-    vector<thread> threads;
-
-    for (int i = 0; i < MAX_CLIENTS; ++i) {
+    // Accept 4 players
+    while (client_sockets.size() < MAX_CLIENTS) {
         new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen);
         if (new_socket < 0) {
             perror("Accept failed");
             exit(EXIT_FAILURE);
         }
-
-        {
-            lock_guard<mutex> lock(clients_mutex);
-            clients.push_back(new_socket);
-        }
-
-        threads.emplace_back(handle_client, new_socket, i);
+        client_sockets.push_back(new_socket);
+        cout << "Player " << client_sockets.size() << " connected.\n";
     }
 
-    // All players connected
-    broadcast_start();
+    // Generate and shuffle deck
+    vector<Card> deck = generateDeck();
 
-    // Join threads (in real game you'd likely keep them running)
-    for (auto& t : threads) t.join();
+    // Deal 13 cards each
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        vector<Card> hand(deck.begin() + (i * 13), deck.begin() + (i + 1) * 13);
+        sendHand(client_sockets[i], hand);
+    }
+
+    cout << "Cards dealt. Game setup complete.\n";
 
     close(server_fd);
     return 0;

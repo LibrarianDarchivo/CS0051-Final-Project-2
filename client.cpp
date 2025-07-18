580af806ce4373b69003e563822fe4ca1a24fb33
Jan Andrei Reyes <167843080+LibrarianDarchivo@users.noreply.github.com>
2025-07-18 21:53:53 +0800

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,47 +1,50 @@
 #include <iostream>
 #include <string>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 #include <unistd.h>
-
-#define PORT 12345
-
+#include <arpa/inet.h>
 using namespace std;
 
-int main() {
-    int sock = 0;
-    sockaddr_in serv_addr;
-    char buffer[4096] = {0};
+const int PORT = 12345;
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+int main() {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         cerr << "Socket creation error\n";
-        return -1;
+        return 1;
     }
 
+    sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    if (inet_pton(AF_INET, "192.168.254.143", &serv_addr.sin_addr) <= 0) {
-        cerr << "Invalid address\n";
-        return -1;
-    }
+    // Replace this with your server's actual IP
+    string server_ip = "192.168.254.143";
+    inet_pton(AF_INET, server_ip.c_str(), &serv_addr.sin_addr);
 
     if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        cerr << "Connection Failed\n";
-        return -1;
+        cerr << "Failed to connect to server\n";
+        return 1;
     }
-
-    cout << "Connected to server.\n";
 
     cout << "Enter your name: ";
     string name;
     getline(cin, name);
-    send(sock, name.c_str(), name.length(), 0);
+    send(sock, name.c_str(), name.size(), 0);
 
-    int valread = read(sock, buffer, sizeof(buffer));
-    if (valread > 0) {
-        cout << string(buffer, valread) << endl;
+    while (true) {
+        char buffer[1024] = {0};
+        int valread = read(sock, buffer, 1024);
+        if (valread <= 0) break;
+
+        string serverMsg(buffer, valread);
+        cout << serverMsg;
+
+        if (serverMsg.find("your turn") != string::npos) {
+            string move;
+            cout << ">> ";
+            getline(cin, move);
+            send(sock, move.c_str(), move.size(), 0);
+        }
     }
 
     close(sock);

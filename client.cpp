6eca6dd686bd85f628ed4a33ebd00cc6c722a3b8
Jan Andrei Reyes <167843080+LibrarianDarchivo@users.noreply.github.com>
2025-07-18 22:01:14 +0800

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,52 +1,42 @@
 #include <iostream>
-#include <string>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <string>
 using namespace std;
 
 const int PORT = 12345;
 
 int main() {
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        cerr << "Socket creation error\n";
-        return 1;
-    }
+    int sock = 0;
+    struct sockaddr_in serv_addr;
 
-    sockaddr_in serv_addr;
+    sock = socket(AF_INET, SOCK_STREAM, 0);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-
-    // Replace this with your server's actual IP
-    string server_ip = "192.168.254.143";
-    inet_pton(AF_INET, server_ip.c_str(), &serv_addr.sin_addr);
-
-    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        cerr << "Failed to connect to server\n";
-        return 1;
-    }
 
     cout << "Enter your name: ";
     string name;
     getline(cin, name);
+
+    inet_pton(AF_INET, "192.168.254.143", &serv_addr.sin_addr); // Replace IP if needed
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        cerr << "Connection failed\n";
+        return 1;
+    }
+
     send(sock, name.c_str(), name.size(), 0);
 
+    char buffer[1024];
     while (true) {
-        char buffer[1024] = {0};
-        int valread = read(sock, buffer, 1024);
+        int valread = read(sock, buffer, sizeof(buffer) - 1);
         if (valread <= 0) break;
-
-        string serverMsg(buffer, valread);
-        cout << serverMsg;
-
-        if (serverMsg.find("your turn") != string::npos) {
-            string move;
-            cout << ">> ";
-            getline(cin, move);
-            send(sock, move.c_str(), move.size(), 0);
-        }
+        buffer[valread] = '\0';
+        cout << buffer;
     }
 
     close(sock);
     return 0;
 }
+
+rizz
